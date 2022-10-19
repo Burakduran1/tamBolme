49f9b1e271691af3d584edd59cf5479c70045a25
Burak Can DURAN <BURAK.DURAN44@hotmail.com>
2022-10-19 22:58:34 +0300

    Add project files.

---
+++ tamBölme/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+double x = Convert.ToDouble(Console.ReadLine());
+double  y= Convert.ToDouble(Console.ReadLine());
+
+if (x % y == 0)
+{
+    Console.WriteLine("X y'ye tam bölünür");
+
+
+}
+else if (x % y !=0)
+{
+    Console.WriteLine("x ' y'ye tam bölünmez");
+
+}
